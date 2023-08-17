bd95704021647be8a509843e243ae6ab7de7fe73
ming-90 <minki319@naver.com>
2023-08-17 22:42:48 +0900

    upload file viewer

--- src/frontend/util.js
+++ src/frontend/util.js
@@ -1,3 +1,46 @@
 const change_image = async (e) => {
-    console.log(e)
+    file_name = e.target.files[0].name
+    $("#input_text").val(file_name)
+    file_type = e.target.files[0].type
+
+    file = e.target.files[0]
+    show_image(file)
+
+    let formData = new FormData()
+    formData.append("image", file, "image.jpg")
+    await fetch("/infer", {
+        method: 'POST',
+        body: formData,
+        headers:{
+            accept: "application/json",
+        }
+    }).then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+    })
+}
+
+const show_image = (file) => {
+    type = file.type
+    if(type.split("/")[0] == "image"){
+        create_tag(".pdf-viewer", "image")
+    }else if(type.split("/")[0] == "application/pdf"){
+        create_tag(".pdf-viewer", "pdf")
+    }
+    url = window.URL.createObjectURL(file)
+    $("#file").attr("src", url)
+}
+
+const create_tag = (parent, type) => {
+    tag = ""
+    if(type == "image"){
+        tag = `
+            <image id=file></image>
+        `
+    }else{
+        tag = `
+            <iframe></iframe>
+        `
+    }
+    $(parent).html(tag)
 }
