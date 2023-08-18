c8c2a378e87e6ebe30269a7e24680b0c5d94bfef
ming-90 <minki319@naver.com>
2023-08-18 11:51:06 +0900

    danfo.js

--- src/frontend/util.js
+++ src/frontend/util.js
@@ -1,3 +1,4 @@
+test = ""
 const change_image = async (e) => {
     file_name = e.target.files[0].name
     $("#input_text").val(file_name)
@@ -17,14 +18,31 @@ const change_image = async (e) => {
         }
     }).then((response) => response.json())
     .then((data) => {
-        console.log(data)
         $.each(data, function(idx, val) {
-            console.log(val)
-            s = new dfd.Series(val)
-            s.print()
+            convert_data = data_convert(val)
+            let col = Object.keys(val)
+            console.log(convert_data)
+            data_frame = new dfd.DataFrame(convert_data, {columns:col})
+            data_frame.print()
+            data_frame.plot("result-viewer").table()
 
         })
     })
+}
+
+const data_convert = (val) => {
+    let col = Object.values(val)
+    let len = Object.values(col[0]).length
+    convert_data = [...Array(len)].map(() => [])
+
+    for(let i=0;i<col.length;i++){
+        let row = Object.values(col[i])
+        for(let j=0;j<row.length;j++){
+            convert_data[j].push(row[j])
+        }
+    }
+
+    return convert_data
 }
 
 const show_image = (file) => {
